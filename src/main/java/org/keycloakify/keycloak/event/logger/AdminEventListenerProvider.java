dabccee50c43bf1d816cd487194eb817118a6e47
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-12-15 18:13:21 +0100

    Simpler implementation

--- src/main/java/org/keycloakify/keycloak/event/logger/AdminEventListenerProvider.java
+++ src/main/java/org/keycloakify/keycloak/event/logger/AdminEventListenerProvider.java
@@ -7,32 +7,11 @@ import org.keycloak.events.admin.AdminEvent;
 public class AdminEventListenerProvider implements EventListenerProvider {
 
     @Override
-    public void onEvent(Event event) { 
-
-        System.out.println("===================================> onEvent(Event event)");
-
-    }
+    public void onEvent(Event event) { }
 
     @Override
     public void onEvent(AdminEvent adminEvent, boolean includeRepresentation) {
-
-        String realmName = null;
-
-        try{
-            realmName = adminEvent.getAuthDetails().getRealmName();
-
-        }catch(Exception e){
-            realmName = "cannot_tell_on_older_keycloak_version";
-        }
-
-        /* 
-        if( realmName.equals("master") ){
-            return;
-        }
-        */
-
-        System.out.println("keycloakify-logging: REALM_CONFIG_CHANGED - realmName=" + realmName);
-
+        System.out.println("keycloakify-logging: REALM_CONFIG_CHANGED");
     }
 
     @Override
